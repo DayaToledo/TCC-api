aecd0666b745adff751531627ddde627b0e27ac1
dayana <dayanabritotoledo_@hotmail.com>
2022-12-21 20:50:22 -0300

    Adjust predict scripts

--- src/scripts/pre_processor.py
+++ src/scripts/pre_processor.py
@@ -5,9 +5,9 @@ import os
 pd.options.display.max_columns = 150
 
 def pre_process():
-    # Lendo o CSV com os dados
+    # Lendo e juntando os CSVs com os dados
     samples_path = os.path.abspath("src/data/samples/")
-    data_set = pd.read_csv(f"{samples_path}/data-final.csv", sep='\t')
+    data_set = pd.read_csv(f"{samples_path}/data.csv", sep=',')
 
     # Removendo valores ausentes
     print("Conjunto de dados: ", data_set.shape)
@@ -21,55 +21,17 @@ def pre_process():
     data_set = data_set.replace(0, np.nan).dropna(axis=0).reset_index(drop=True)
     remove_rows = start_rows - len(data_set)
 
-    # Define os labels positivos
-    positively_keyed = [
-        'EXT1', 'EXT3', 'EXT5', 'EXT7', 'EXT9',                             #5 extroversão
-        'EST1', 'EST3', 'EST5', 'EST6', 'EST7', 'EST8', 'EST9', 'EST10',    #8 neuroticismo                            
-        'AGR2', 'AGR4', 'AGR6', 'AGR8', 'AGR9', 'AGR10',                    #6 Compatibilidade
-        'CSN1', 'CSN3', 'CSN5', 'CSN7', 'CSN9', 'CSN10',                    #6 Responsabilidade
-        'OPN1', 'OPN3', 'OPN5', 'OPN7', 'OPN8', 'OPN9', 'OPN10'             #7 Abertura à experiência
-    ]
-
-    # Define os labels negativos
-    negatively_keyed = [
-        'EXT2', 'EXT4', 'EXT6', 'EXT8', 'EXT10',    #5 extroversão
-        'EST2', 'EST4',                             #2 neuroticismo
-        'AGR1', 'AGR3', 'AGR5', 'AGR7',             #4 Compatibilidade
-        'CSN2', 'CSN4', 'CSN6', 'CSN8',             #4 Responsabilidade
-        'OPN2', 'OPN4', 'OPN6'                      #3 Abertura à experiência
-    ]
-
-    # Subtraindo 6 de cada item negativo, para termos uma direção dos dados
-    data_set.loc[:, negatively_keyed] = 6 - data_set.loc[:, negatively_keyed]
+    # data_set = data_set.sample() 
 
     # Definindo e ordenando as colunas
-    columns = positively_keyed + negatively_keyed
-    data_set = data_set[sorted(columns)]
-
-    # Definindo as labels a serem usadas a seguir
     personality_traits = ["EXT", "AGR", "CSN", "EST", "OPN"]
+    personality_result = 'personality'
 
-    # Soma os valores correspondentes a cada personalidade listada acima, para cada uma das linhas, 
-    # e divide por 10, que é a quantia de perguntas respondidas para aquele traço de personalidade
-    personality_traits_columns = []
-    for trait in personality_traits:
-        personality_traits_columns = sorted([col for col in data_set.columns if trait in col and '_E' not in col])
-        data_set[trait] = data_set[personality_traits_columns].sum(axis=1).div(10)
+    y = data_set[personality_result]
+    X = data_set[sorted(personality_traits)]
 
-    # Soma o total da pontuação das personalidades
-    data_set["SUM"] = data_set[personality_traits].sum(axis=1)
-
-    # Calcula a porcentagem de cada traço da personalidade
-    for trait in personality_traits:
-        data_set[trait + "_PER"] = data_set[trait].mul(100).div(data_set["SUM"])
-
-    personality_traits_final = ["EXT_PER", "AGR_PER", "CSN_PER", "EST_PER", "OPN_PER"]
-
-    y = data_set[personality_traits_final]
-    X = data_set[sorted(columns)]
-    # print(y)
     # print(X)
-
+    # print(y)
     return X, y
 
 def get_x(data_set):
@@ -118,10 +80,8 @@ def get_x(data_set):
     personality_traits_columns = []
     for trait in personality_traits:
         personality_traits_columns = sorted([col for col in data_set.columns if trait in col and '_E' not in col])
-        data_set[trait] = data_set[personality_traits_columns].sum(axis=1).div(10)
+        data_set[trait] = data_set[personality_traits_columns].sum(axis=1).floordiv(10)
 
-    X = data_set[sorted(columns)]
-    # print(y)
-    # print(X)
-
+    X = data_set[personality_traits]
+    X = np.array(X)
     return X
--- src/scripts/train.py
+++ src/scripts/train.py
@@ -1,9 +1,10 @@
 import numpy as np
 import os
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
+from sklearn.metrics import accuracy_score
+import pickle
 
-from model import get_model, compile_model, train_model
 from pre_processor import pre_process
 
 np_array_path = os.path.abspath("src/data/np_arrays/")
@@ -18,30 +19,16 @@ if not (
     np.save(f"src/data/np_arrays/X.npy", X)
     np.save(f"src/data/np_arrays/y.npy", y)
 
-X = np.load(f"src/data/np_arrays/X.npy")
-y = np.load(f"src/data/np_arrays/y.npy")  
+X = np.load(f"src/data/np_arrays/X.npy", allow_pickle=True)
+y = np.load(f"src/data/np_arrays/y.npy", allow_pickle=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 1)
 
-model = get_model(X_train)
-model = compile_model(model)
-train_model(X_train, X_test, y_train, y_test, model)
+model = LogisticRegression(multi_class='multinomial', solver='newton-cg', max_iter=100)
+model.fit(X_train, y_train)
 
-loss, Y1_loss ,Y2_loss, Y3_loss, Y4_loss, Y5_loss, Y1_rmse, Y2_rmse, Y3_rmse, Y4_rmse, Y5_rmse = model.evaluate(X_test, y_test)
+predict = model.predict(X_test)
+accuracy = accuracy_score(y_test, predict) * 100
+print ("The accuracy was {:.2f}%.".format(accuracy))
 
-print() 
-print(f'loss: {loss}') 
-print() 
-print(f'EXT_PER_loss: {Y1_loss}') 
-print(f'AGR_PER_loss: {Y2_loss}') 
-print(f'CSN_PER_loss: {Y3_loss}') 
-print(f'EST_PER_loss: {Y4_loss}') 
-print(f'OPN_PER_loss: {Y5_loss}') 
-print() 
-print(f'EXT_PER_rmse: {Y1_rmse}') 
-print(f'AGR_PER_rmse: {Y2_rmse}')
-print(f'CSN_PER_rmse: {Y3_rmse}')
-print(f'EST_PER_rmse: {Y4_rmse}')
-print(f'OPN_PER_rmse: {Y5_rmse}')
-
-model.save("src/data/models/model.h5")
-print("Model sucessfully trained!")
+pickle.dump(model, open("src/data/models/model.pkl", 'wb'))
+print("Model sucessfully trained and saved!")
